265e327d61b1a8ab1cea85d77d8c940bfab8222e
SHADOW383-svg <simonmuesseler@web.de>
2025-11-04 18:22:28 +0100

    changed logo

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FlutterLogo(style: FlutterLogoStyle.stacked);
+    return FlutterLogo(style: FlutterLogoStyle.stacked, size: 100);
   }
 }

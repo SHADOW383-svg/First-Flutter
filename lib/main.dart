d6bd56bc887c74a7c95a34e99fc6e0769ad5a150
SHADOW383-svg <simonmuesseler@web.de>
2025-11-05 19:22:16 +0100

    Own Scaffold bulidingTest

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,34 @@ void main() {
   runApp(const MyApp());
 }
 
+//Globales WIdget -> beschreibt festgelegte Parameter der App im ganzen
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FlutterLogo(style: FlutterLogoStyle.stacked, size: 100);
+    return MaterialApp(
+      title: 'Deine App', //Name der App für das Gerät
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ), //Gloable/Standart Farbe der App
+      home:
+          const MyHomePage(), //Welche Seite/Widget beim starten der App gezeigt wird
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello User!"),
+        backgroundColor: Color.fromARGB(48, 255, 1, 1),
+        centerTitle: true,
+      ),
+    );
   }
 }

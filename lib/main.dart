aefaa5fa97820d8e35472acd13e8d6a391536df9
SHADOW383-svg <simonmuesseler@web.de>
2025-11-16 16:35:25 +0100

    animatead Button

--- lib/main.dart
+++ lib/main.dart
@@ -21,55 +21,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Liste mit Farben, die durchgewechselt werden sollen
+  final List<Color> circleColors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+  ];
+
+  int currentIndex = 1; // Startfarbe
+
+  void changeColor() {
+    setState(() {
+      currentIndex = (currentIndex + 1) % circleColors.length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello User!"),
-        backgroundColor: Color.fromARGB(250, 3, 210, 100),
+        title: const Text("Hello User!"),
+        backgroundColor: const Color.fromARGB(250, 3, 210, 100),
         centerTitle: true,
       ),
       body: Column(
-        children: <Widget>[
+        children: [
           Center(
-            child: Column(
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  padding: EdgeInsetsGeometry.all(35),
-                  margin: EdgeInsets.all(10),
-                  alignment: Alignment.center,
-
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.yellow.withValues(),
-                        blurRadius: 1,
-                        spreadRadius: 2,
-                        offset: const Offset(2, 4),
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Das ist ein Container!",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color.fromARGB(250, 0, 0, 0),
-                        fontSize: 25,
-                      ),
+            child: GestureDetector(
+              onTap: changeColor,
+              child: AnimatedContainer(
+                duration: const Duration(seconds: 2),
+                width: 200,
+                height: 200,
+                padding: const EdgeInsets.all(35),
+                margin: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: circleColors[currentIndex],
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.yellow.withValues(),
+                      blurRadius: 1,
+                      spreadRadius: 2,
+                      offset: const Offset(2, 4),
                     ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    "Tippe mich!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 25),
                   ),
                 ),
-              ],
+              ),
             ),
           ),
+
           Container(
             //color: Color.fromARGB(248, 13, 184, 24),
             width: 300,

65af597df5e1c0b1e7dee4fe140073b384892050
SHADOW383-svg <simonmuesseler@web.de>
2025-12-03 15:30:58 +0100

    Blueprint ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 // Globales Widget -> beschreibt festgelegte Parameter der App im ganzen
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(title: 'My App', home: HomePage());
@@ -16,50 +15,59 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  final List<Color> colorList = List.generate(12, (_) => Colors.teal);
+class ChangeValue1 extends ChangeNotifier {
+  int _count = 0;
+  int get count => _count;
+  void increaseValue() {
+    _count++;
+    notifyListeners();
+  }
+}
 
-  void toggleTile(int index) {
-    setState(() {
-      colorList[index] = colorList[index] == Colors.teal
-          ? Colors.red
-          : Colors.teal;
-    });
+class _HomePageState extends State<HomePage> {
+  late ChangeValue1 changeValue0;
+  @override
+  void initState() {
+    super.initState();
+    changeValue0 = ChangeValue1();
+  }
+
+  @override
+  void dispose() {
+    changeValue0.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("What ever"),
+        title: const Text("What ever"),
         backgroundColor: Colors.cyan,
         centerTitle: true,
       ),
-      body: GridView.builder(
-        itemCount: colorList.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 1,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () => toggleTile(index),
-            child: Container(
-              color: colorList[index],
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              child: Text("Hello World"),
-            ),
-          );
-        },
+      body: Column(
+        children: [
+          ListenableBuilder(
+            listenable: changeValue0,
+            builder: (context, child) {
+              return Text(
+                'counter: ${changeValue0.count}',
+                style: const TextStyle(fontSize: 22),
+              );
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              changeValue0.increaseValue();
+            },
+            child: const Text("Does it work?"),
+          ),
+        ],
       ),
     );
   }
